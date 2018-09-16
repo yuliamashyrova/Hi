e8a83956fcb75cf8a14d5699ad306d74041f4539
Юлия <yulia-mashyrova@mail.ru>
2018-09-16 10:47:29 +0300

    lll

--- src/Str.java
+++ src/Str.java
@@ -2,6 +2,8 @@ public class Str {
     public static void main(String[] args)
     {
         System.out.println("Hi!");
+        System.out.println("Hi!");
+        System.out.println("Hi!");
     }
 
 
